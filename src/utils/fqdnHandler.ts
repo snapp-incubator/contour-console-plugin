f7cf4aa1d3fea5eeea2d4353352b4b0af7de0c7c
Meysam Jafari <meysam.jeffrey@gmail.com>
2024-08-20 22:46:29 +0330

    feat: update fqdnHandler function

--- src/utils/fqdnHandler.ts
+++ src/utils/fqdnHandler.ts
@@ -3,30 +3,27 @@ const domainRegex = /^([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,}$/;
 
 export const convertToDomain = (value: string): string => {
   if (value != null) {
+    // Regular expression for valid domain
+    const domainRegex = /^([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,}$/;
+
     // Check if it's a valid domain
     if (domainRegex.test(value)) {
       return value;
     }
+
     const parts = value.split('.');
     if (parts.length > 0) {
       const hostname = window.location.hostname;
-      return `${value}.${hostname}`;
+      // Remove 'console' from the hostname
+      const cleanedHostname = hostname.replace(/^console\./, '');
+      return `${value}.${cleanedHostname}`;
     }
   }
-
   return value;
 };
 
 export const convertToString = (value: string): string => {
   if (value != null) {
-    // Regular expression for valid domain
-    const hostname = window.location.hostname;
-    const parts = value.split('.');
-
-    if (parts.length > 0 && parts[1] === hostname) {
-      return parts[0];
-    }
-
     // Check if it's a valid domain
     if (domainRegex.test(value)) {
       return value;
